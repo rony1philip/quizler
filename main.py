089eed24b4f37c116cdd188448c4be87a3834bd0
rony <26e4u69@gmail.com>
2024-01-30 20:30:20 +0200

    dev2

--- main.py
+++ main.py
@@ -10,14 +10,16 @@ quiz = QuizBrain()
 question_data = quiz.question_data
 
 
+
 for question in question_data:
     question_text = question["question"]
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
+
 quiz.question_data = question_bank
 
-quiz_ui = QuizInterface(quiz)
+quiz_ui = QuizInterface(quiz,new_question)
 
 
 print("You've completed the quiz")
--- quiz_brain.py
+++ quiz_brain.py
@@ -1,10 +1,12 @@
 import html
 import requests
 
+
 class QuizBrain:
 
     def __init__(self):
         self.question_number = 0
+        self.question_number_t = 0
         self.score = 0
         self.current_question = None
         self.index = 0
@@ -13,22 +15,29 @@ class QuizBrain:
         self.parameters = {'amount': 10, 'category': 18, 'difficulty': self.difficulty[self.index], 'type': 'boolean'}
         self.question_data = self.get_data()
 
+
     def get_data(self):
         response = requests.get(url='https://opentdb.com/api.php', params=self.parameters)
         response.raise_for_status()
         question_data = response.json()
         self.question_data = question_data['results']
-        print(self.difficulty[self.index])
+        print(f'{0} {self.question_data}')
         return self.question_data
 
     def still_has_questions(self):
         return self.question_number < len(self.question_data)
 
     def next_question(self):
+        self.question_num_config()
         self.current_question = self.question_data[self.question_number]
         self.question_number += 1
+        self.question_number_t += 1
         q_text = html.unescape(self.current_question.text)
         return f"Q.{self.question_number}: {q_text}"
+
+    def question_num_config(self):
+        if self.question_number_t == 10 or self.question_number_t == 20:
+            self.question_number = 0
 
     def check_answer(self):
         correct_answer = self.current_question.answer
